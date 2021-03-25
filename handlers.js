2e3f08d007e6773e3882fa297bc3c922c203bfec
Andrey Vasilenko <vasilenkoa0101@gmail.com>
2021-03-25 22:41:21 +0300

    prototype is working

--- handlers.js
+++ handlers.js
@@ -8,8 +8,210 @@ const url = require('url');
 // "database"
 Accounts = {}
 
-// classes
-const CurrentAccount = require('./classes.js')
+// Классы
+// Текущий счет
+class CurrentAccount {
+	constructor(clientID, ID, currency) {
+		this.clientID = clientID
+		this.ID = ID
+		this.currency = currency
+
+		this.balance = 0
+		this.type = "Current"
+
+		this.reserved = false
+		this.opened = false
+		this.closed = false
+	}
+	// резервирование
+	reserve(){
+		if(this.reserved == false){
+			this.reserved = true
+			return {
+				status: true,
+				msg: "Success! The bank account (ID:"+ this.ID +") was successfully reserved"
+			}
+		} else {
+			return {
+				status: false,
+				msg: 'Error! The bank account (ID:'+ this.ID +') already passed the process of reservation'
+			}
+		}
+	}
+	// открытие
+	open(){
+		if(this.opened == false){
+			if (this.reserved == true){
+				this.opened = true
+				console.log("Success! The bank account (ID:"+ this.ID +") was successfully opened.")
+				return {
+					status: true,
+					msg: "Success! The bank account (ID:"+ this.ID +") was successfully opened."
+				}
+			} else {
+				console.log("Error! The bank account (ID:"+ this.ID +") must be reserved before.")
+				return {
+					status: false,
+					msg: "Error! The bank account (ID:"+ this.ID +") must be reserved before."
+				}
+			}
+		} else {
+			console.log("Error! The bank account (ID:"+ this.ID +") was already opened.")
+			return {
+				status: false,
+				msg: "Error! The bank account (ID:"+ this.ID +") was already opened."
+			}
+		}
+	}
+	// пополнение
+	topUp(amount){
+		if (this.closed == false){
+			if (this.opened == true){
+				this.balance += parseInt(amount,10)
+				console.log("Success! The bank account (ID:"+ this.ID +") was topped up. Current balance: " + this.balance.toString() + " " + this.currency.toString())
+				return {
+					status: true,
+					msg: "Success! The bank account (ID:"+ this.ID +") was topped up. Current balance: " + this.balance.toString() + " " + this.currency.toString()
+				}
+			} else {
+				console.log("Error! The bank account (ID:"+ this.ID +") must be opened before.")
+				return {
+					status: false,
+					msg: "Error! The bank account (ID:"+ this.ID +") must be opened before."
+				}
+			}
+		} else {
+			return {
+				status: false,
+				msg: "Error! The bank account (ID:"+ this.ID +") closed."
+			}
+		}
+		
+	}
+	// списание
+	withdraw(amount){
+		if (this.closed == false){
+			if (this.opened == true){
+				this.balance -= parseInt(amount,10)
+				console.log("Success! The money has been withdrawn from the bank account (ID:"+ this.ID +"). Current balance: " + this.balance.toString() + " " + this.currency.toString())
+				return {
+					status: true,
+					msg: "Success! The money has been withdrawn from the bank account (ID:"+ this.ID +"). Current balance: " + this.balance.toString() + " " + this.currency.toString()
+				}
+			} else {
+				console.log("Error! The bank account (ID:"+ this.ID +") must be opened before.")
+				return {
+					status: false,
+					msg: "Error! The bank account (ID:"+ this.ID +") must be opened before."
+				}
+			}
+		} else {
+			return {
+				status: false,
+				msg: "Error! The bank account (ID:"+ this.ID +") closed."
+			}
+		}
+		
+	}
+	// закрытие
+	close(){
+		if (this.closed == false){
+			if (this.opened == true){
+				this.closed = true
+				console.log("Success! The bank account (ID:"+ this.ID +") was successfully closed.")
+				return {
+					status: true,
+					msg: "Success! The bank account (ID:"+ this.ID +") was successfully closed."
+				}
+			} else {
+				console.log("Error! The bank account (ID:"+ this.ID +") must be opened before.")
+				return {
+					status: false,
+					msg: "Error! The bank account (ID:"+ this.ID +") must be opened before."
+				}
+			}
+		} else {
+			console.log('Error! The bank account (ID:'+ this.ID +') already closed.')
+			return {
+				status: false,
+				msg: 'Error! The bank account (ID:'+ this.ID +') already closed.'
+			}
+		}
+	}
+
+}
+
+// Депозит до востребования
+class DemandDeposit extends CurrentAccount {
+	constructor(clientID, ID, currency, interestRate) {
+		super(clientID)
+		this.ID = ID
+		this.currency = currency
+
+		this.interestRate = interestRate
+		this.type = "DemandDeposit"
+	}
+	// начисление процентов
+	interestCharge(){
+		if (this.closed == false){
+			if (this.opened == true){
+				return {
+					status: true,
+					msg: "Success! The interest was charged."
+				}
+			} else {
+				return {
+					status: false,
+					msg: "Error! The bank account (ID:"+ this.ID +") must be opened before."
+				}
+			}
+		} else {
+			return {
+				status: false,
+				msg: "Error! The bank account (ID:"+ this.ID +") closed."
+			}
+		}
+	}
+
+	// уплата процентов
+	interestPay(){
+		if (this.closed == false){
+			if (this.opened == true){
+				return {
+					status: true,
+					msg: "Success! The interest was paid."
+				}
+			} else {
+				return {
+					status: false,
+					msg: "Error! The bank account (ID:"+ this.ID +") must be opened before."
+				}
+			}
+		} else {
+			return {
+				status: false,
+				msg: "Error! The bank account (ID:"+ this.ID +") closed."
+			}
+		}
+	}
+}
+
+// Срочный депозит
+class FixedTermDeposit extends DemandDeposit {
+	constructor(clientID, ID, currency, interestRate, closureDate) {
+		super(clientID)
+		this.ID = ID
+		this.currency = currency
+
+		this.interestRate = interestRate
+		this.type = "FixedTermDeposit"
+
+		this.closureDate = closureDate
+	}
+	// hide() {
+	// 	alert(`${this.name} прячется!`);
+	// }
+}
 
 // container
 var handlers = {}
@@ -45,15 +247,54 @@ handlers.createAccount = function(req,res){
 		let clientID = req.body.clientID
 		let type = req.body.type
 		let currency = req.body.currency
-		
+		let interestRate
+		let newAccount
 		if (ID && clientID && type && currency !== undefined){
+
+			switch (type){
+				case "Current":
+					newAccount = new CurrentAccount(clientID, ID, currency)
+
+					Accounts[newAccount.ID] = newAccount
+					console.log("New account (ID:"+ ID +") was successfully created!")
+					res.status(201).send("New account (ID:"+ ID +") was successfully created!")
+					break
+				case "DemandDeposit":
+					interestRate = req.body.interestRate
+					if (interestRate !== undefined && interestRate > 0){
+
+						newAccount = new DemandDeposit(clientID, ID, currency, interestRate)
+
+						Accounts[newAccount.ID] = newAccount
+						console.log("New account (ID:"+ ID +") was successfully created!")
+						res.status(201).send("New account (ID:"+ ID +") was successfully created!")
+						break
+					} else {
+						res.status(400).send("Error! 'interestRate' value must be a positive number.")
+					}
+					
+				case "FixedTermDeposit":
+					interestRate = req.body.interestRate
+					let closureDate = req.body.closureDate
+					if (interestRate !== undefined && interestRate > 0 && closureDate !== undefined){
+
+						newAccount = new FixedTermDeposit(clientID, ID, currency, interestRate, closureDate)
+
+						Accounts[newAccount.ID] = newAccount
+						console.log("New account (ID:"+ ID +") was successfully created!")
+						res.status(201).send("New account (ID:"+ ID +") was successfully created!")
+						break
+					} else {
+						res.status(400).send("Error! 'interestRate' value must be a positive number and closureDate must not be empty.")
+					}
+					
+				default: 
+					res.status(400).send("Error! Wrong account type. Correct values are: \n 'Current', 'DemandDeposit', 'FixedTermDeposit'")
+			}
 			// switch (type)
 			// current => new Account(clientID, ID, currency)
-			let newAccount = new CurrentAccount(clientID, ID, currency)
-			Accounts[newAccount.ID] = newAccount
 
-			console.log("New account (ID:"+ ID +") was successfully created!")
-			res.status(201).send("New account (ID:"+ ID +") was successfully created!")
+			
 		} else {
 			console.log("Error! Account was not created. Check the required values.")
 			res.status(400).send("Error! Account was not created. Check the required values.")
@@ -118,6 +359,24 @@ handlers.editAccount = function(req,res){
 					} else {
 						res.status(400).send("Error! Wrong amount value.")
 					}				
+					break
+
+				case "interestCharge":
+					operationResponse = account.interestCharge()
+					if(operationResponse.status == true){
+						res.status(200).send(operationResponse.msg)
+					} else {
+						res.status(403).send(operationResponse.msg)
+					}
+					break
+
+				case "interestPay":
+					operationResponse = account.interestPay()
+					if(operationResponse.status == true){
+						res.status(200).send(operationResponse.msg)
+					} else {
+						res.status(403).send(operationResponse.msg)
+					}
 					break
 
 				case "close":
